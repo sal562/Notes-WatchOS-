92c2f2635964200dbc7a7866cfcdae61708697a3
Sal <salmanamer@gmail.com>
2020-06-16 03:58:00 -0700

    Initial Commit

---
+++ NotesApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NotesApp
+//
+//  Created by Sal B Amer on 6/16/20.
+//  Copyright © 2020 Sal B Amer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
